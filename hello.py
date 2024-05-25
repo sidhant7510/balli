038e4dfc7a5156831fe1df6ad46455b45a51fc9f
varinder kumar <uncleji372@gmail.com>
2024-05-25 22:50:25 +0000

    it is my today work

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ html>
 aws is server platform .</li>
 <br>please help me 
 i need your help
+Data Lifecycle Manager default policies backup all volumes and instances in this Region that do not have recent backups. You can exclude volumes and instances if needed.
 </body>
 
 
